280d138429e0a526a0feb4613be1cb79848fe89d
Milan Straka <fox@ucw.cz>
2015-02-10 08:34:30 +0100

    Add pugixml 1.5.
    The pugixml is slightly modified:
    - it is placed in ufal::microrestd namespace
    - the name matching ignores XML namespaces
    - XPATH and STL modules are removed

---
+++ gen/pugixml/gen.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+
+wget https://github.com/zeux/pugixml/releases/download/v1.5/pugixml-1.5.tar.gz
+tar xf pugixml-1.5.tar.gz
+rm pugixml-1.5.tar.gz
+
+SRC=pugixml-*/src
+TGT=../../src/pugixml
+
+for f in pugiconfig.h pugixml.h pugixml.cpp; do
+  # Copy the file to TGT with given header
+  cat >$TGT/$f <<EOF
+// Modified by Milan Straka <straka@ufal.mff.cuni.cz>
+// Changes: - added ufal::microrestd namespace
+//          - matching changed to ignore XML namespaces
+//          - XPATH and STL module completely removed
+
+EOF
+  cat $SRC/$f* >>$TGT/$f
+
+  # Remove the XPATH module
+  perl -pe '
+    if (/#if.*NO_(XPATH|STL)/) {
+      print;
+      print "// Removed the XPATH and STL module, by Milan Straka\n";
+      my $stack = 1;
+      while (<>) {
+        $stack++ if /#if/;
+        $stack-- if /#endif/;
+        last unless $stack;
+      }
+    }
+  ' -i $TGT/$f
+
+  # Apply patch
+  patch $TGT/$f $f.patch
+done
+
+echo All done.
+rm -rf pugixml-1.5/
