fb945a43c01f24716a7188efa284a62613b82400
Milan Straka <fox@ucw.cz>
2015-02-19 09:34:57 +0100

    Add json_builder_test.

---
+++ tests/json_builder_test.cpp
@@ -0,0 +1,26 @@
+// This file is part of MicroRestD <http://github.com/ufal/microrestd/>.
+//
+// Copyright 2015 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <cstdio>
+
+#include "microrestd.h"
+
+using namespace ufal::microrestd;
+
+int main(void) {
+  json_builder json;
+
+//  json.object();
+//  json.close_all(true);
+
+  auto data = json.current();
+  printf("%*s", int(data.len), data.str);
+
+  return 0;
+}
